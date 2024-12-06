ff64c256d7feb22601b93f84dc35a26b21aebd04
Dandwate <anant.dandwate@capgemini.com>
2024-12-06 14:58:22 +0100

    Changes CartItem

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -2,11 +2,13 @@ import React, { useState, useEffect } from "react";
 import "./ProductList.css";
 import CartItem from "./CartItem";
 import { addItem } from "./CartSlice";
+import { useSelector, useDispatch } from "react-redux";
 
 function ProductList() {
   const [showCart, setShowCart] = useState(false);
   const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
   const [addedToCart, setAddedToCart] = useState({});
+  const dispatch = useDispatch();
 
   const plantsArray = [
     {
@@ -361,6 +363,10 @@ function ProductList() {
                       alt={plant.name}
                     />
                     <div className="product-title">{plant.name}</div>
+                    <div className="product-description">
+                      {plant.description}
+                    </div>
+                    <div className="product-cost">{plant.cost}</div>
                     {/*Similarly like the above plant.name show other details like description and cost*/}
                     <button
                       className="product-button"
